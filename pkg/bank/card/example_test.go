26a24944b700026fdfacf2e50a548305c220283d
Nekruz <nekruzihikmatullo@gmail.com>
2021-12-14 04:36:30 +0300

    добавлен пакет card и тесты к нему

---
+++ pkg/bank/card/example_test.go
@@ -0,0 +1,19 @@
+package card
+
+import (
+	"bank/pkg/bank/types"
+	"fmt"
+)
+
+func ExampleTotal() {
+	cards := []types.Card{
+		{
+			Balance:1_000_00,
+			Active: true,
+		},
+	}
+
+	fmt.Println(Total(cards))
+	// Output: 100000
+
+}
